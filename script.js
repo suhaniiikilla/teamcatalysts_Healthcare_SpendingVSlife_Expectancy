ca6bde478dab00cdbde70f6ba9962a6481c0d067
suhaniiikilla <ku2407u382@karnavatiuniversity.edu.in>
2024-12-04 22:39:39 +0530

    updated frontend

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 // Dataset
 const dataset = [
-    { entity: "Afghanistan", year: 2021, life_expectancy: 65.1, health_expenditure: 30.5, population: 38041754 },
-    { entity: "Albania", year: 2021, life_expectancy: 78.7, health_expenditure: 840.2, population: 2837743 },
-    { entity: "Algeria", year: 2021, life_expectancy: 76.2, health_expenditure: 350.1, population: 43851044 },
-    { entity: "Afghanistan", year: 2020, life_expectancy: 64.8, health_expenditure: 29.8, population: 37590000 },
-    { entity: "Albania", year: 2020, life_expectancy: 78.5, health_expenditure: 820.1, population: 2829000 },
-    { entity: "Central African Republic", year: 2020, life_expectancy: ,53.6, health_expenditure: 820.1, population: 4829764},
-    { entity: "Central African Republic", year: 2020, life_expectancy: ,53.6, health_expenditure: 820.1, population: 4829764},
-    { entity: "Central African Republic", year: 2020, life_expectancy: ,53.6, health_expenditure: 820.1, population: 4829764},
-    { entity: "Central African Republic", year: 2020, life_expectancy: ,53.6, health_expenditure: 820.1, population: 4829764},
-
-];
+    const dataset = [
+        { entity: "Afghanistan", year: 2021, life_expectancy: 65.1, health_expenditure: 30.5, population: 38041754 },
+        { entity: "Albania", year: 2021, life_expectancy: 78.7, health_expenditure: 840.2, population: 2837743 },
+        { entity: "Algeria", year: 2021, life_expectancy: 76.2, health_expenditure: 350.1, population: 43851044 },
+        { entity: "Afghanistan", year: 2020, life_expectancy: 64.8, health_expenditure: 29.8, population: 37590000 },
+        { entity: "Albania", year: 2020, life_expectancy: 78.5, health_expenditure: 820.1, population: 2829000 },
+        { entity: "Central African Republic", year: 2020, life_expectancy: 53.6, health_expenditure: 820.1, population: 4829764 },
+        { entity: "Hungary", year: 2020, life_expectancy: 75.6, health_expenditure: 1676, population: 9660350 },
+        { entity: "Italy", year: 2020, life_expectancy: 82.34, health_expenditure: 800.1, population: 60461828 },
+        { entity: "Japan", year: 2020, life_expectancy: 84.62, health_expenditure: 4256, population: 126476458 },
+        { entity: "Malta", year: 2020, life_expectancy: 82.65, health_expenditure: 2900, population: 441539 },
+        { entity: "New Zealand", year: 2021, life_expectancy: 81.9, health_expenditure: 3500, population: 4860642 },
+        { entity: "Palestine", year: 2021, life_expectancy: 72.5, health_expenditure: 760, population: 5222756 },
+        { entity: "India", year: 2020, life_expectancy: 69.89, health_expenditure: 56, population: 1380004385 },
+        { entity: "Guyana", year: 2020, life_expectancy: 70.02, health_expenditure: 267.8, population: 786559 },
+        { entity: "Kenya", year: 2021, life_expectancy: 64.3, health_expenditure: 63, population: 54985702 }
+    ];
+    
 
 // Populate dropdowns
 const yearSelect = document.querySelector("#year-select");
